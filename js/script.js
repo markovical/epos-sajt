3fa00918728eacf48d6da4d28de74fb9ce99785b
markovical <44428138+markovical@users.noreply.github.com>
2019-11-24 18:52:50 +0100

    prva verzija
    prva verzija

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $("#nanoGallery1").nanoGallery({
+		thumbnailWidth:200,
+		thumbnailHeight:200,
+        items: [
+            {
+                src: 'img/gallery/1.jpg',
+                srct: 'img/gallery/1.jpg',
+                title: 'image 1',
+                description : 'image 1'		
+			},
+			{
+                src: 'img/gallery/2.jpg',
+                srct: 'img/gallery/2.jpg',
+                title: 'image 2',
+                description : 'image 2'		
+            },
+			{
+                src: 'img/gallery/3.jpg',
+                srct: 'img/gallery/3.jpg',
+                title: 'image 3',
+                description : 'image 3'		
+            },
+			{
+                src: 'img/gallery/4.jpg',
+                srct: 'img/gallery/4.jpg',
+                title: 'image 4',
+                description : 'image 4'		
+            },
+			{
+                src: 'img/gallery/5.jpg',
+                srct: 'img/gallery/5.jpg',
+                title: 'image 5',
+                description : 'image 5'		
+            },
+			{
+                src: 'img/gallery/6.jpg',
+                srct: 'img/gallery/6.jpg',
+                title: 'image 6',
+                description : 'image 6'		
+            },
+			{
+                src: 'img/gallery/7.png',
+                srct: 'img/gallery/7.png',
+                title: 'image 7',
+                description : 'image 7'		
+			}
+        ]
+    });
+  });
